b2ebce6750c5eb1fff9f5928f2743b7da282bc62
Lars Munch <lm@napatech.com>
2018-04-09 14:18:56 +0200

    Added requirements.txt

--- env.sh
+++ env.sh
@@ -3,11 +3,7 @@
 if [ ! -d venv3 ]; then
     python3 -m venv venv3
     venv3/bin/pip install --upgrade pip
-    venv3/bin/pip install lxml
-    venv3/bin/pip install pytz
-    venv3/bin/pip install requests
-    venv3/bin/pip install six
-    venv3/bin/pip install -I garmin-uploader==1.0.4
+    venv3/bin/pip install -r requirements.txt
 fi
 
 source venv3/bin/activate
